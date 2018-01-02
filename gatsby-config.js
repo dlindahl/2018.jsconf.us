7ca1df0cae4f7d135ff743e2d732c7e340844f40
Derek Lindahl <derek.lindahl@gmail.com>
2018-01-02 10:41:37 -0800

    Add gatsby-plugin-offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
     },
     `gatsby-plugin-netlify`,
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sitemap`
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-offline`
   ],
 }
