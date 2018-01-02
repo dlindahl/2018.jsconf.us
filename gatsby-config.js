198a2bf1889fffe24ddcd43edb1aeb575830a28d
Derek Lindahl <derek.lindahl@gmail.com>
2018-01-02 11:06:53 -0800

    Add gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,29 @@ module.exports = {
     },
     `gatsby-plugin-netlify`,
     `gatsby-plugin-react-helmet`,
+    {
+    options: {
+      background_color: `#f0db4f`,
+      display: 'standalone',
+      icons: [
+        {
+          sizes: '192x192',
+          src: '/img/favicons/android-chrome-192x192.png',
+          type: 'image/png'
+        },
+        {
+          sizes: '512x512',
+          src: '/img/favicons/android-chrome-512x512.png',
+          type: 'image/png'
+        }
+      ],
+      name: `JSConf US`,
+      short_name: `JSConf US`,
+      start_url: '/',
+      theme_color: `#f0db4f`
+    },
+    resolve: 'gatsby-plugin-manifest'
+  },
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-offline`
   ],
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import Head from 'react-helmet'
 import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
+import PropTypes from 'prop-types'
+import React from 'react'
 
 import './index.css'
 
@@ -36,13 +36,24 @@ const Header = () => (
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
+    <Head>
+      <html lang="en"/>
+      <meta content="text/html;charSet=UTF-8" httpEquiv="Content-type"/>
+      <meta content="ie=edge" httpEquiv="x-ua-compatible"/>
+      <title>JSConf US 2018</title>
+      <meta content="2018 iteration of JSConf US" name="description"/>
+      <meta
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        name="viewport"
+      />
+      <link rel="apple-touch-icon" sizes="180x180" href="/img/favicons/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/img/favicons/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/img/favicons/favicon-16x16.png"/>
+      <link rel="mask-icon" href="/img/favicons/safari-pinned-tab.svg" color="#f0db4f"/>
+      <link rel="shortcut icon" href="/img/favicons/favicon.ico"/>
+      <meta name="msapplication-config" content="/img/favicons/browserconfig.xml"/>
+      <title>JSConf US 2018</title>
+    </Head>
     <Header />
     <div
       style={{
