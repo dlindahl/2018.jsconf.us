ef96c4178bce6e5a9027d676e1254cc8d84533a8
Derek Lindahl <derek.lindahl@gmail.com>
2018-03-29 00:10:13 -0700

    Refactor GridRow to seperate header from content
    Splitting out the block content from the header allows some more
    flexibility in the grid system when it comes to things like adding
    full-width images for content blocks.
    
    This required some maneuvering of styles, especially in regards to
    Markdown-driven content.

--- src/components/SchedulePage/Campfires.js
+++ src/components/SchedulePage/Campfires.js
@@ -1,9 +1,13 @@
+import GridRow from '../GridRow'
 import React from 'react'
 
 const Campfires = () => (
-  <div>
-    <h2>Poolside Campfire</h2>
-    <h3>9:00PM - 11:00PM</h3>
+  <GridRow
+    className="Schedule-Event"
+    level={2}
+    subtitle="9:00PM - 11:00PM"
+    title="Poolside Campfires"
+  >
     <p>
       Whether you just need a break from the dinner or are looking for deep
       conversation, stop by the evening campfire located on the beach. Pull up
@@ -11,7 +15,7 @@ const Campfires = () => (
       with your fellow attendees for the next or plan your space mission to the
       moon and back!
     </p>
-  </div>
+  </GridRow>
 )
 
 export default Campfires
