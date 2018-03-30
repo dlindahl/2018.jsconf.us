f9f21813a7512afa2e5d1ba2d7c1d2f770a2ffd6
Derek Lindahl <derek.lindahl@gmail.com>
2018-03-29 22:46:34 -0700

    Fix wording of Campfire content to be less Florida and more California

--- src/components/SchedulePage/Campfires.js
+++ src/components/SchedulePage/Campfires.js
@@ -6,10 +6,10 @@ const Campfires = () => (
     <h3>9:00PM - 11:00PM</h3>
     <p>
       Whether you just need a break from the dinner or are looking for deep
-      conversation, stop by the evening campfire located on the beach. Pull up
-      some sand, roast a couple marshmallows, and enjoy the stars. Make plans
-      with your fellow attendees for the next or plan your space mission to the
-      moon and back!
+      conversation, stop by the evening campfires located on the upper-level of
+      the Edge Pool. Grab a spot around the natural gas firepits and enjoy some
+      enlightened conversation. Make plans with your fellow attendees for the
+      next day or plan your space mission to the moon and back!
     </p>
   </div>
 )
