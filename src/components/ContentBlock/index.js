ad4fa567e6238e8a41e745b7d4fa18cdee5b0e96
Derek <derek.lindahl@gmail.com>
2018-04-18 00:29:30 -0700

    Add GridLayout support to ContentBlock

--- src/components/ContentBlock/index.js
+++ src/components/ContentBlock/index.js
@@ -1,21 +1,35 @@
-import { any, arrayOf, bool, string } from 'prop-types'
+import { any, arrayOf, bool, object, string } from 'prop-types'
 import ButtonGroup from '../ButtonGroup'
 import ContentBlockFocus from './ContentBlockFocus'
 import ContentBlockTitle from './ContentBlockTitle'
 import GridRow from '../GridRow'
+import GridLayout from '../GridLayout'
 import React from 'react'
 import { slugify } from '../../util/string'
 
 import './ContentBlock.css'
 
-const ContentBlock = ({ actions, children, feature, focus, title }) => (
+const ContentBlock = ({
+  actions,
+  children,
+  feature,
+  focus,
+  images,
+  layout,
+  title
+}) => (
   <GridRow className="ContentBlock" id={slugify(title)}>
-    <ContentBlockTitle feature={feature}>{title}</ContentBlockTitle>
-    <div className="ContentBlock-Content">
-      <ContentBlockFocus>{focus}</ContentBlockFocus>
-      {children}
-    </div>
-    <ButtonGroup>{actions}</ButtonGroup>
+    <GridRow.NarrowContent>
+      <ContentBlockTitle feature={feature}>{title}</ContentBlockTitle>
+      <div className="ContentBlock-Content">
+        <ContentBlockFocus>{focus}</ContentBlockFocus>
+        {children}
+      </div>
+      <ButtonGroup>{actions}</ButtonGroup>
+    </GridRow.NarrowContent>
+    <GridRow.WideContent>
+      <GridLayout images={images} layout={layout}/>
+    </GridRow.WideContent>
   </GridRow>
 )
 
@@ -24,6 +38,8 @@ ContentBlock.propTypes = {
   children: any,
   feature: bool,
   focus: string,
+  images: arrayOf(object),
+  layout: string,
   title: string.isRequired
 }
 
