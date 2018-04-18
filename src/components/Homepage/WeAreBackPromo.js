7c0d7890dc44c27cb55bff0250a5645687cb3fd0
Derek <derek.lindahl@gmail.com>
2018-04-18 00:34:20 -0700

    Add California photos to WeAreBackPromo

--- src/components/Homepage/WeAreBackPromo.js
+++ src/components/Homepage/WeAreBackPromo.js
@@ -2,6 +2,24 @@ import ContentBlock from '../ContentBlock'
 import LinkButton from '../LinkButton'
 import React from 'react'
 
+const IMAGES = [
+  {
+    alt: '',
+    caption: 'Photo by Andrew Bui on Unsplash',
+    src: '/img/homepage/pier.jpg'
+  },
+  {
+    alt: '',
+    caption: 'Photo by Adele Payman on Unsplash',
+    src: '/img/homepage/vines.jpg'
+  },
+  {
+    alt: '',
+    caption: 'Photo by Robert Trombetta on Unsplash',
+    src: '/img/homepage/gg.jpg'
+  }
+]
+
 const WeAreBackPromo = (props) => {
   const actions = [
     <LinkButton href="/tickets/" key={0} primary>
@@ -12,7 +30,13 @@ const WeAreBackPromo = (props) => {
     </LinkButton>
   ]
   return (
-    <ContentBlock actions={actions} title="We Are Back!" {...props}>
+    <ContentBlock
+      actions={actions}
+      images={IMAGES}
+      layout="111"
+      title="We Are Back!"
+      {...props}
+    >
       The JSConf US is back for 2018! We've moved across country and are
       celebrating everything West Coast and California Dreamin'.
     </ContentBlock>
