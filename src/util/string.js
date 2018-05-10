82e197fe349eea18d727f88733e995b8079d91c1
Derek Lindahl <derek.lindahl@gmail.com>
2018-05-10 13:49:48 -0700

    Add ability to call toSentence on JSX strings

--- src/util/string.js
+++ src/util/string.js
@@ -1,4 +1,6 @@
-export function slugify (str) {
+const React = require('react')
+
+function slugify (str) {
   return str
     .trim()
     .toLowerCase()
@@ -6,12 +8,44 @@ export function slugify (str) {
     .replace(/([^a-zA-Z0-9._-]+)/, '')
 }
 
-export function toSentence (strs) {
-  if (strs.length === 2) {
-    return strs.join(' and ')
+function toSentenceJSX (items) {
+  items = React.Children.toArray(items)
+  if (items.length === 2) {
+    items.splice(1, 0, ' and ')
+    return items
   }
-  if (strs.length > 2) {
-    return `${strs.slice(0, -1).join(', ')}, and ${strs.slice(-1)}`
+  const lastItem = items.pop()
+  const set = items.reduce(
+    (sentence, item) => sentence.concat([item, ', ']),
+    []
+  )
+  return set.concat([' and ', lastItem])
+}
+
+function toSentence (items) {
+  if (items.length < 2) {
+    return items
   }
-  return strs
+  if (React.isValidElement(items[0])) {
+    return toSentenceJSX(items)
+  }
+  if (items.length === 2) {
+    return items.join(' and ')
+  }
+  items = items.slice()
+  const lastItem = items.pop()
+  return `${items.join(', ')} and ${lastItem}`
+}
+
+function pluralize (set, singular, plural) {
+  if (set.length === 1) {
+    return singular
+  }
+  return plural
+}
+
+module.exports = {
+  pluralize,
+  slugify,
+  toSentence
 }
