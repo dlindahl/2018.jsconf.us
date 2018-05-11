f74614c2253e2ad593d64f688bb141f6b1f5628e
Derek <derek.lindahl@gmail.com>
2018-05-10 20:42:21 -0700

    Fix production build issue when building Schedule Nav
    Searching for nodes based on the displayName doesn't work in prod
    because displayName is stripped. This meant no dates were found so
    when formatted, dateformat used the same default value for everything.

--- src/components/SchedulePage/ScheduleNav.js
+++ src/components/SchedulePage/ScheduleNav.js
@@ -5,11 +5,25 @@ import React, { Children } from 'react'
 import './ScheduleNav.css'
 import { slugify } from '../../util/string'
 
+function extractDates (children) {
+  return Children.toArray(children).map((item) => {
+    const retval = [item.props.title]
+    const timetable = Children.toArray(item.props.children).find(
+      (child) => child.props.date
+    )
+    if (timetable) {
+      retval.push(timetable.props.date)
+    }
+    return retval
+  })
+}
+
 function componentize (schedule) {
   const [title, date] = schedule
   return (
     <li className="Schedule-NavItem" key={title}>
       <a className="Schedule-NavLink" href={`#${slugify(title)}`}>
+        {date} ==
         <span className="Schedule-LinkDate">{dateformat(date, 'UTC:m/d')}</span>
         <span className="Schedule-LinkTitle">{title}</span>
       </a>
@@ -18,16 +32,7 @@ function componentize (schedule) {
 }
 
 const ScheduleNav = ({ children }) => {
-  const schedules = Children.toArray(children).map((item) => {
-    const retval = [item.props.title]
-    const timetable = Children.toArray(item.props.children).find(
-      (child) => child.type.displayName === 'TimeTable'
-    )
-    if (timetable) {
-      retval.push(timetable.props.date)
-    }
-    return retval
-  })
+  const schedules = extractDates(children)
   return (
     <div>
       <GridRow className="Schedule-NavRow">
