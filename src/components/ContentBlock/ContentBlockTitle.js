7001c12c6c65fe88f7fd9cc2712df9708b5a8dcb
Derek <derek.lindahl@gmail.com>
2018-05-15 21:51:26 -0700

    Add slug generation to ContentBlock titles

--- src/components/ContentBlock/ContentBlockTitle.js
+++ src/components/ContentBlock/ContentBlockTitle.js
@@ -1,7 +1,7 @@
-import { any, bool } from 'prop-types'
+import { bool, string } from 'prop-types'
 import classnames from 'classnames'
 import React from 'react'
-
+import { slugify } from '../../util/string'
 import './ContentBlockTitle.css'
 
 const ContentBlockTitle = ({ children, feature }) => {
@@ -12,11 +12,15 @@ const ContentBlockTitle = ({ children, feature }) => {
     ContentBlockTitle: true,
     'ContentBlockTitle--feature': feature
   })
-  return <h2 className={cls}>{children}</h2>
+  return (
+    <h2 className={cls} id={slugify(children)}>
+      {children}
+    </h2>
+  )
 }
 
 ContentBlockTitle.propTypes = {
-  children: any,
+  children: string,
   feature: bool
 }
 
